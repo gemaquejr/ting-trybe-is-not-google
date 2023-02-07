80151ff4faac20011d409f7ee560a20305516c5a
gemaquejr <gemaquejr@hotmail.com>
2023-02-06 21:04:08 -0300

    Adiciona requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith('txt'):
+        return sys.stderr.write('Formato inválido\n')
+    try:
+        with open(path_file, 'r') as file:
+            content = file.read()
+            list = content.split('\n')
+            return list
+
+    # Ref: Course e https://learnpython.com/blog/python-standard-error-stream/
+
+    except OSError:
+        return sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
