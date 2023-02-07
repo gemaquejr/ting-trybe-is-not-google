5bb44c365ebe0a75b924b36c3b341512c9c03f75
gemaquejr <gemaquejr@hotmail.com>
2023-02-07 12:06:16 -0300

    Adiciona requisito 5

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -26,4 +26,9 @@ def remove(instance):
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        file = instance.search(position)
+        return sys.stdout.write(str(file))
+
+    except IndexError:
+        return sys.stderr.write('Posição inválida\n')
