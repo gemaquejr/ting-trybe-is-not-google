1cbec41e1a2ead8d2a439d45b8384c023cce1844
gemaquejr <gemaquejr@hotmail.com>
2023-02-08 14:29:14 -0300

    Adiciona requisito 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,20 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    list = []
+    search = []
+
+    for index, line in enumerate(instance.queue[0]["linhas_do_arquivo"]):
+        if word.casefold() in line.casefold():
+            list.append({"linha": index + 1})
+
+    for index in range(len(instance.queue)):
+        if len(list) > 0:
+            search.append({
+                "palavra": word,
+                "arquivo": instance.queue[index]["nome_do_arquivo"],
+                "ocorrencias": list
+            })
+
+    return search
 
 
 def search_by_word(word, instance):
